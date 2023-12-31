cab6c06008289de94a9a28b65b65b4abc10f55b7
ngtrgiabao <ngtrgiabao251002@gmail.com>
2023-12-31 14:32:42 +0700

    update

--- src/app/modules/feed/post-list/post-preview/post-preview.component.ts
+++ src/app/modules/feed/post-list/post-preview/post-preview.component.ts
@@ -1,17 +1,19 @@
 import { ApiService } from 'src/app/helpers/api.service';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-post-preview',
   templateUrl: './post-preview.component.html',
 })
 export class PostPreviewComponent implements OnInit {
-  @Input('posts') posts: any = [];
+  @Input() postImages!: any;
   
   constructor(private apiService: ApiService) {    
-  }  
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+  }
 
   ngOnInit(): void {
-
   }  
 }
